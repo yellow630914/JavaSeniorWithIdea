ca4e1ec4ec2402033352c35a9086a2a25a694321
yellow630914 <39198808+yellow630914@users.noreply.github.com>
2022-02-08 23:15:47 +0800

    0208

--- src/com/lijen/multithreading/HelloWorld.java
+++ src/com/lijen/multithreading/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("Hello");
+
+        System.out.println("shit");
     }
 }
